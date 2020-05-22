956393bdc0d1a872711164162e7e508ad6704797
khwanchai-phaipha <porpan00@hotmail.com>
2020-05-22 14:01:38 -0400

    sprint05-22-2020

--- challenges/closure.js
+++ challenges/closure.js
@@ -17,8 +17,19 @@ function myFunction() {
 myFunction();
 
 // Explanation: 
-
+`Because of nestedFunction is the child function fo myFunction, the Child function alwasy can get what ever Parent function have.
+or other words the it pass from outside to inside never inside to outside`
 
 /* Task 2: Counter */
 
 /* Create a function called `sumation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+let count = 4;
+function sumation(){
+  
+  return function counter(){
+    for (let i = 0; i <=count; i++)
+     console.log (count++);
+  }
+   
+}
+sumation();
